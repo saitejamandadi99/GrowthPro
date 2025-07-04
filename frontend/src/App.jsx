0a961678a6405eb393a7cd5e1a057e0db1d0ca7b
saitejamandadi99 <saitejamandadi99>
2025-07-04 18:52:32 +0530

    input form completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+import BusinessForm from './components/BusinessForm'
+
+function App() {
+  return (
+    <div>
+      <h1>This is Growth API</h1>
+      <BusinessForm />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/BusinessForm.jsx
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+
+const BusinessForm = ({onSubmit}) =>{
+    const [name, setName] = useState('');
+    const [location, setLocation] = useState('');
+    const [isLoading, setLoading] = useState(false);
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(!name || !location){
+            alert('Please fill all fields');
+            return;
+        }
+        setLoading(true);
+        const formData = {name, location};
+        onSubmit(formData);
+    }
+
+    const handleNameChange = (e) =>{
+        setName(e.target.value);
+    }
+    const handleLocationChange = (e) =>{
+        setLocation(e.target.value);
+    }
+
+
+    return(
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto bg-white p-6 rounded-2xl shadow-md space-y-4">
+      <h2 className="text-xl font-semibold text-center">Business Info</h2>
+
+      <div>
+        <label className="block text-gray-700 font-medium mb-1">Business Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+          className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="e.g., Cake & Co"
+        />
+      </div>
+
+      <div>
+        <label className="block text-gray-700 font-medium mb-1">Location</label>
+        <input
+          type="text"
+          name="location"
+          value={location}
+          onChange={handleLocationChange}
+          className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="e.g., Mumbai"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition"
+        disabled={isLoading}
+      >
+        {isLoading ? 'Loading...' : 'Get Reviews'} 
+      </button>
+    </form>
+    )
+
+}
+
+export default BusinessForm;
