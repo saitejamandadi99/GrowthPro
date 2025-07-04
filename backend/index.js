ec66c61164bc233590c9ef6815459701618925a2
saitejamandadi99 <saitejamandadi99>
2025-07-04 12:20:23 +0530

    added routes to index

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,14 @@ app.use(express.json());
 
 app.use(cors());
 
+
+
+app.get('/', (req,res)=>{
+    res.send('Welcome to GrowthPro API')
+})
+
+app.use('/api', require('./routes/businessRoutes'));
+
 app.listen(5000, () =>{
-    console.log('Server is running on port 5000');
+    console.log('Server is running on http://localhost:5000');
 })
